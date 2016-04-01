b0db08416b94bbcd58a8f04da17a41c0eaa98355
Brent Mills <mills.brent@gmail.com>
2016-04-01 14:49:10 -0500

    changed environment variable names

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Run a single node kafka instance connecting to zk on the same machine
 
 usage:
-docker run -d -p 9092:9092 -e ZK_IP=192.168.99.100 -e KAFKA_IP=192.168.99.100 bmills/kafka
+docker run -d -p 9092:9092 -e ZK_HOST=zookeeper01.cloud.uship.com -e KAFKA_HOST=kafka01.cloud.uship.com bmills/kafka
 
--- startKafka.sh
+++ startKafka.sh
@@ -1,4 +1,4 @@
-sed -i s/REPLACEME_ZOOKEEPER/$ZK_IP/ config/server.properties
-sed -i s/REPLACEME_KAFKA/$KAFKA_IP/ config/server.properties
+sed -i s/REPLACEME_ZOOKEEPER/$ZK_HOST/ config/server.properties
+sed -i s/REPLACEME_KAFKA/$KAFKA_HOST/ config/server.properties
 
 ./bin/kafka-server-start.sh config/server.properties
